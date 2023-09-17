dc5e09e9efb994bbb7a319ad8e311c02e8a7c5a5
cafe small house <ybaram40@gmail.com>
2023-09-18 08:05:29 +0900

    confirm 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
 import { useAtom } from 'jotai';
 import { atomWithObservable } from 'jotai/utils';
 import { Subject } from 'rxjs';
-
+import { confirmAlert } from 'react-confirm-alert';
 const subject = new Subject<number>();
 const counterAtom = atomWithObservable(() => subject, {
   initialValue: 10,
 });
 
 function App() {
+  const submit = () => {
+    confirmAlert({
+      title: 'Confirm to submit',
+      message: 'Are you sure to do this.',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => setCounter(counter + 1),
+        },
+        {
+          label: 'No',
+          onClick: () => alert('Click No'),
+        },
+      ],
+    });
+  };
+
   const [counter, setCounter] = useAtom(counterAtom);
 
   return (
     <>
       <div>count: {counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>+1</button>
+      <button
+        onClick={() => {
+          submit();
+        }}
+      >
+        +1
+      </button>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import Modal from 'react-modal';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 Modal.setAppElement('#root');
 ReactDOM.createRoot(document.getElementById('root')!).render(
